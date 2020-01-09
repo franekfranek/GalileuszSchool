d385e6c0494d1da1c51c2c815ed9d693cdaaaa6b
franekfranek <franciszekezawadzki@gmail.com>
2020-01-09 23:40:44 +0100

    Admin Pages Details

--- GalileuszSchool/Areas/Admin/Controllers/PagesController.cs
+++ GalileuszSchool/Areas/Admin/Controllers/PagesController.cs
@@ -18,7 +18,7 @@ namespace GalileuszSchool.Areas.Admin.Controllers
         {
             this.context = context;
         }
-
+        // /admin/pages
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
@@ -26,6 +26,18 @@ namespace GalileuszSchool.Areas.Admin.Controllers
 
 
             return View(pagesList);
+        }
+        // /admin/pages/details/{id}
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
         }
     }
 }
