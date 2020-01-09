2b1639cd69ae27cbda1e6586c3c501b0051ce164
franekfranek <franciszekezawadzki@gmail.com>
2020-01-09 21:31:07 +0100

    pacages installed and Db context

---
+++ GalileuszSchool/Infrastructure/GalileuszSchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalileuszSchool.Infrastructure
+{
+    public class GalileuszSchoolContext : DbContext
+    {
+        public GalileuszSchoolContext(DbContextOptions<GalileuszSchoolContext> options) : base(options)
+        {
+        }
+
+
+
+    }
+}
