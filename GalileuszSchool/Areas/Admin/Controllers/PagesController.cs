18d872b44f00f59dffba20ab42c1c66f2e7ea6ee
franekfranek <franciszekezawadzki@gmail.com>
2020-01-09 23:33:36 +0100

    Admin Pages Index List

---
+++ GalileuszSchool/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GalileuszSchool.Infrastructure;
+using GalileuszSchool.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalileuszSchool.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly GalileuszSchoolContext context;
+
+        public PagesController(GalileuszSchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+
+
+            return View(pagesList);
+        }
+    }
+}
--- GalileuszSchool/Startup.cs
+++ GalileuszSchool/Startup.cs
@@ -55,6 +55,10 @@ namespace GalileuszSchool
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
