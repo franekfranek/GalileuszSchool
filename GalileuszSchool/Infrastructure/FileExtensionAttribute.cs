70e3d5185deabbbb3481f540b82fd35183f76a0f
franekfranek <franciszekezawadzki@gmail.com>
2020-01-13 08:12:10 +0100

    studnent index create/edit/delete

---
+++ GalileuszSchool/Infrastructure/FileExtensionAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCart.Infrastructure
+{
+    public class FileExtensionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+
+                string[] extensions = { "jpg", "png" };
+                bool result = extensions.Any(x => extension.EndsWith(x));
+
+                if (!result)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage()
+        {
+            return "Allowed extensions are .jpg and .png";
+        }
+    }
+}
--- GalileuszSchool/wwwroot/js/site.js
+++ GalileuszSchool/wwwroot/js/site.js
@@ -12,3 +12,15 @@
         }, 2000);
     }
 });
+
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("img#imgupload").attr("src", e.target.result).width(200).height(200);
+        };
+        reader.readAsDataURL(input.files[0])
+    }
+}
