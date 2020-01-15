04508dde011584011038bd6bded0af551f021838
franekfranek <franciszekezawadzki@gmail.com>
2020-01-15 01:24:59 +0100

    admin/roles index/create post get and edit get roles

--- GalileuszSchool/Areas/Admin/Controllers/RolesController.cs
+++ GalileuszSchool/Areas/Admin/Controllers/RolesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using GalileuszSchool.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace GalileuszSchool.Areas.Admin.Controllers
 {
@@ -21,9 +22,67 @@ namespace GalileuszSchool.Areas.Admin.Controllers
             this.userManager = userManager;
               
         }
+
+        // get /admin/Roles
         public IActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+
+        //get /admin/roles/create
+        public IActionResult Create()
         {
             return View();
         }
+
+        //post /admin/roles/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([MinLength(2), Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    TempData["Success"] = "The role has been created";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Minimum lenght is 2");
+            return View();
+        }
+
+        //get /admin/roles/edit{id}
+        
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+
+
+            foreach (AppUser user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+
+            return View(new RoleEdit
+            {
+                Role = role,
+                Memmbers = members,
+                NonMemmbers = nonMembers
+            });
+        }
+
     }
 }
---
+++ GalileuszSchool/Models/RoleEdit.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalileuszSchool.Models
+{
+    public class RoleEdit
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Memmbers { get; set; }
+        public IEnumerable<AppUser> NonMemmbers { get; set; }
+
+        public string RoleName { get; set; }
+
+        public string[] AddIds { get; set; }
+        public string[] DeleteIds { get; set; }
+    }
+}
