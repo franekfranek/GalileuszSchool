82686f12d4a19b9b72c6b8cbed809b8cb6502355
franekfranek <franciszekezawadzki@gmail.com>
2020-06-06 12:55:43 +0200

    admin/users/index added , table,  delete, and details of users

---
+++ GalileuszSchool/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using GalileuszSchool.Infrastructure;
+using GalileuszSchool.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GalileuszSchool.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private IPasswordHasher<AppUser> _passwordHasher;
+        private readonly GalileuszSchoolContext _context;
+        
+        public UserController(UserManager<AppUser> userManager,
+                                SignInManager<AppUser> signInManager,
+                                IPasswordHasher<AppUser> passwordHasher,
+                                GalileuszSchoolContext context
+                                )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _passwordHasher = passwordHasher;
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> GetUsers()
+        {
+            List<AppUser> users = await _context.Users.OrderByDescending(x => x.Id).ToListAsync();
+            return Json(users);
+        }
+
+        public async Task<IActionResult> FindUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return new JsonResult(user);
+        }
+
+        //get/admin/teachers/delete/{id}
+        public async Task<IActionResult> Delete(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json(new { text = "User does not exists!" });
+            }
+            else
+            {
+                await _userManager.DeleteAsync(user);
+            }
+
+            return Ok();
+        }
+    }
+
+}
---
+++ GalileuszSchool/wwwroot/js/userIndex.js
@@ -0,0 +1,132 @@
+﻿$(document).ready(function () {
+    GetUsers();
+    //console.log("checking...")
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //------------->DETAILS USER
+    $(document).on('click', '#detailsUser', function () {
+        var userId = $(this).data('user-id');
+        var userName = $(this).data('user-name');
+
+        document.getElementById("detailsModalTitle").innerHTML = "User " +  userName + " details";
+
+        $.ajax({
+            type: 'Get',
+            data: { id: userId },
+            url: '/admin/User/FindUser',
+            success: function (result) {
+                $('#detailsUserModal #id').text(result.id);
+                $('#detailsUserModal #phoneNumberConfirmed').text(result.PhoneNumberConfirmed);
+                $('#detailsUserModal #accessFailedCount').text(result.AccessFailedCount);
+                $('#detailsUserModal #securityStamp').text(result.SecurityStamp);
+            }
+        })
+    });
+
+
+    //////------------->DELETE
+    $(document).on('click', '#deleteUserLink', function () {
+
+        var userId = $(this).data('user-id');
+        var userName = $(this).data('user-name');
+
+
+        var x = $('#userModal').text("Remove user " + userName + " ?");
+        console.log(userName);
+
+        $('#deleteUserModal #deleteUserId').val(userId);
+        $('#deleteUserModal #deleteUserName').val(userName);
+    });
+
+    $('#deleteUser').on('click', function (e) {
+
+        e.preventDefault();
+        var userId = $('input#deleteUserId').val();
+        var userName = $('#deleteUserName').val();
+        console.log(userId);
+        console.log(userName);
+
+
+        $.ajax({
+            type: 'GET',
+            data: { id: userId },
+            url: '/admin/User/Delete',
+            success: function () {
+                $('#deleteUserModal').modal('hide');
+
+                var notf = $(document).find('#divNotification');
+                notf.attr("class", "alert alert-warning notification");
+                notf.html("You removed " + userName + " !").show();
+                setTimeout(function () {
+                    notf.hide("slow");
+                }, 2000);
+                GetUsers();
+            },
+            error: function (response) {
+                $('#deleteUserModal').modal('hide');
+
+                var notf = $(document).find('#divNotification');
+                notf.attr("class", "alert alert-danger notification");
+                notf.html(response.responseJSON.text).show();
+                setTimeout(function () {
+                    notf.hide("slow");
+                }, 2000);
+                GetUsers();
+            }
+
+        })
+    });
+
+    
+});
+
+var GetUsers = function () {
+
+    $.ajax({
+        type: "post",
+        url: "/Admin/User/GetUsers",
+        dataType: 'json',
+        success: function (data) {
+            bindDataTable(data);
+        }
+    })
+};
+
+var table;
+var bindDataTable = function (data) {
+    if ($.fn.dataTable.isDataTable("#usersTable")) {
+        $("#usersTable").DataTable()
+            .clear()
+            .rows.add(data)
+            .draw();
+    } else {
+
+
+        table = $("#usersTable").dataTable({
+
+            data: data,
+            columns: [
+
+                { 'data': 'userName' },
+                { 'data': 'isStudent' },
+                { 'data': 'phoneNumber' },
+                { 'data': 'email' },
+                { 'data': 'emailConfirmed' },
+
+                {
+                    data: null, render: function (data, type, row) {
+                        return '<a href="#detailsUserModal" id="detailsUser" data-toggle="modal" data-user-id="' + data.id + '"data-user-name='
+                            + data.userName + '><i class="material-icons" data-toggle="tooltip" title="Details">&#xE8D2;</i></a>';
+                    }
+                },
+                {
+                    data: null, render: function (data, type, row) {
+                        return '<a href="#deleteUserModal" id="deleteUserLink" data-toggle="modal" data-user-id="' + data.id + '"data-user-name='
+                            + data.userName + '><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>';
+                    }
+                },
+            ]
+        });
+    }
+}
+
